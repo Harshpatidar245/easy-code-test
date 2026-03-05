08113827099215e6b301bce3f41dbd81eb29fd97
nihaalsharma2000 <nihaalsharma2000@gmail.com>
2026-03-05 16:04:27 +0530

    checkout UI

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ import WhyEasyCodeSection from "./components/WhyEasyCodeSection";
 import BonusesSection from "./components/BonusesSection";
 import ProgramRoadmapSection from "./components/ProgramRoadmapSection";
 import Footer from "./components/Footer";
-
 export default function Home() {
   return (
     <div className="min-h-screen bg-white">
